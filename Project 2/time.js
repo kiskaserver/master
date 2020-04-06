441677b24b84ec17856f77032c6d2d25925ef2c2
kiskaserver <32369874+kiskaserver@users.noreply.github.com>
2020-04-06 22:20:40 +0300

    Add files via upload

---
+++ Project 2/time.js
@@ -0,0 +1,38 @@
+/*
+let time = prompt("Input time: ");
+if (time >= 17)
+{
+	alert("Добрый вечер!");
+} else {
+	alert("Добрый день!");
+}
+*/
+/*
+let name = prompt("Input name: ");
+if (name == "John")
+{
+	alert("Привет " + name);
+} else {
+	alert("Я вас не знаю");
+}
+*/
+/*
+let login = prompt("Input login: ");
+let pass = prompt("Input password: ");
+if (login == "ivan" && pass == "333")
+{
+	alert("Доброе пожаловать " + login);
+} else if (login == "ssss" && pass == "666") {
+	alert("Доброе пожаловать " + login);	
+} else if (login == "gibs" && pass == "0000") {
+	alert("Доброе пожаловать " + login);
+} else {
+	alert("Пользователь " + login + " не найден!");
+}
+*/
+/*
+let a = prompt("Введите число 1: ");
+let b = prompt("Введите число 2: ");
+let c = prompt("Введите число 3: ");
+alert(Math.max(a,b,c));
+*/
